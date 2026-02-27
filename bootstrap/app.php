8f4a277e006ce47e7ec66d9c36129a351fa09cb8
sanjohn <sanjohn.in@gmail.com>
2026-02-27 20:40:52 +0700

    update latest

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
-        \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
-        \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
-        \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
-        \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
-    );
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
