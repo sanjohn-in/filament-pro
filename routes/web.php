7a9cecd23fd383b85f015b116f4fd90cfe64bcc9
sanjohn <sanjohn.in@gmail.com>
2026-02-28 12:00:17 +0700

    fix web route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    $host = Request::getHost();
+
+    if (str_contains($host, 'pro')) {
+        return redirect('/admin');
+    }
+    return redirect('/app');
 });
 
