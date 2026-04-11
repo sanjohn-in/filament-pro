74ea193503af207739e36e1f4fcf47813a1a241d
sanjohn <sanjohn.in@gmail.com>
2026-04-11 14:51:33 +0700

    fix font weight

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1091,7 +1091,7 @@
     <section class="section-dark py-5 px-4">
         <div class="max-w-3xl mx-auto text-center">
 
-            <h2 class="reveal mt-2 font-bold" style="font-size:1.5rem">
+            <h2 class="reveal mt-2 f-serif " style="font-size:1.5rem">
                 {{ $translations['event_info'] }}
             </h2>
 
